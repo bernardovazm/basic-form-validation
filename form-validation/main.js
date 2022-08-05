5db05451e80ca47d06f01877f82691f685c39813
Bernardo Vaz <77335626+bernardovazm@users.noreply.github.com>
2022-08-05 08:26:42 -0300

    First commit

---
+++ form-validation/main.js
@@ -0,0 +1,27 @@
+
+// declarating and getting elements
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let username = id("username"),
+  email = id("email"),
+  password = id("password"),
+  form = id("form"),
+
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+let engine = (id, serial, message) => {
+
+  if (id.value.trim() === "") {
+  }
+
+  else {
+  }
+}
